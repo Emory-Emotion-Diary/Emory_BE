9858b38f32031ae16d33aaf38f915b2f22d64129
troy96475 <taesupark0504@gmail.com>
2025-04-16 14:55:47 +0900

    first commit

---
+++ src/main/java/com/example/emory/EmoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.emory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emory/EmoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
