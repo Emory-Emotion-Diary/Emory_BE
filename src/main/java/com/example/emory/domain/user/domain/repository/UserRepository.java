f09a4421a68eedac44621d6545d9459f98f1b2ed
박태수 <taesupark0504@gmail.com>
2025-06-18 23:30:23 +0900

    feat :: userRepository

---
+++ src/main/java/com/example/emory/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.emory.domain.user.domain.repository;
+
+import com.example.emory.domain.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+
+}
