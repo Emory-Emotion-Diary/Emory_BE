873ddec5c1cccaf5348db5ab42c82f6b602be11f
박태수 <taesupark0504@gmail.com>
2025-06-18 23:32:43 +0900

    feat :: tokenResponse

---
+++ src/main/java/com/example/emory/domain/auth/domain/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,15 @@
+package com.example.emory.domain.auth.domain.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@Builder
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+    private Date accessExpiredAt;
+    private Date refreshExpiredAt;
+}
