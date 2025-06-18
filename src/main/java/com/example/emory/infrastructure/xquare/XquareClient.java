d43d0c618854b6c2190a5295003fb0e656d1d3bb
박태수 <taesupark0504@gmail.com>
2025-06-18 23:18:11 +0900

    feat :: xquareClient

---
+++ src/main/java/com/example/emory/infrastructure/xquare/XquareClient.java
@@ -0,0 +1,16 @@
+package com.example.emory.infrastructure.xquare;
+
+import com.example.emory.domain.auth.domain.presentation.dto.request.LoginRequest;
+import com.example.emory.infrastructure.xquare.dto.XquareUserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "xquare-client", url = "${key.login-api-url}", configuration = XquareRetryConfiguration.class)
+public interface XquareClient {
+    @PostMapping("/user-data")
+    XquareUserResponse xquareUser(@RequestBody LoginRequest request);
+
+    @PatchMapping("/modify-profile")
+    void modifyProfile(@RequestParam("account_id") String account_id, @RequestParam("profile_img_url") String profileImgUrl);
+}
+
