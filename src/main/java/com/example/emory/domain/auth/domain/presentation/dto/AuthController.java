fadfb52d8af75591f643acc7182624dec49baef2
박태수 <taesupark0504@gmail.com>
2025-06-18 23:36:35 +0900

    feat :: login api

---
+++ src/main/java/com/example/emory/domain/auth/domain/presentation/dto/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.emory.domain.auth.domain.presentation.dto;
+
+import com.example.emory.domain.auth.domain.presentation.dto.request.LoginRequest;
+import com.example.emory.domain.auth.domain.presentation.dto.response.TokenResponse;
+import com.example.emory.domain.auth.service.LoginService;
+import com.example.emory.domain.auth.service.ReissueService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final LoginService loginService;
+    private final ReissueService reissueService;
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public TokenResponse login(@RequestBody @Valid LoginRequest request) {
+        return loginService.login(request);
+    }
+
+
+}
